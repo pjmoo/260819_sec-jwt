25d821b03e4e717a5f8a3db0264fbe1ddd7d11da
pjmoo <00pjmoooo@gmail.com>
2026-08-19 11:03:55 +0900

    feat: AuthProperties에 JWT 설정 레코드(Jwt) 추가

--- src/main/java/org/example/secjwt/auth/AuthProperties.java
+++ src/main/java/org/example/secjwt/auth/AuthProperties.java
@@ -2,9 +2,14 @@ package org.example.secjwt.auth;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+import java.time.Duration;
+
 @ConfigurationProperties(prefix = "app.auth")
-public record AuthProperties (
+public record AuthProperties(
         // encoding-id -> encodingId
-        String encodingId
+        String encodingId,
+        Jwt jwt
 ) {
+    public record Jwt(String secret, Duration accessExpiry) {
+    }
 }
