f02eb12a61ab3c2eb29900c60e44a45a72441b1a
pjmoo <00pjmoooo@gmail.com>
2026-08-19 10:17:13 +0900

    feat: SecurityFilterChain 기본 보안 설정 및 접근 허용 경로 추가

--- src/main/java/org/example/secjwt/auth/SecurityConfig.java
+++ src/main/java/org/example/secjwt/auth/SecurityConfig.java
@@ -4,11 +4,15 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 import java.util.Map;
 
@@ -19,7 +23,30 @@ public class SecurityConfig {
     private final AuthProperties p; // 주입받을 수 있음
 
     // SecurityFilterChain
-
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) {
+        // http...
+        http
+                // .cors...
+                .csrf(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                // 경로 보안
+                .authorizeHttpRequests(
+                        authz -> authz
+                                .requestMatchers(
+                                        "swagger-ui/**",
+                                        "/v3/api-docs/**",
+                                        "/swagger-ui.html")
+                                .permitAll()
+                                .requestMatchers(
+                                        HttpMethod.POST, "/user/signup")
+                                .permitAll()
+                );
+        // 예외 처리
+        // .exceptionHandling()
+        return http.build();
+    }
 
     // PasswordEncoder - 가입하고 DB 저장될 때 해싱해서 암호화된 비밀번호를 저장
     @Bean
