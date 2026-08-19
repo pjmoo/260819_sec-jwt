fbcc09e0c471612b73b6f3efea3c71c1c82d89f5
pjmoo <00pjmoooo@gmail.com>
2026-08-19 11:12:49 +0900

    feat: JJWT 라이브러리 추가 및 Access Token 발급 로직 구현

--- src/main/java/org/example/secjwt/auth/JwtProvider.java
+++ src/main/java/org/example/secjwt/auth/JwtProvider.java
@@ -1,12 +1,32 @@
 package org.example.secjwt.auth;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
 
 @Component
 @EnableConfigurationProperties(AuthProperties.class)
 @RequiredArgsConstructor
 public class JwtProvider {
     private final AuthProperties p;
+
+    public SecretKey getSecretKey() {
+        return Keys.hmacShaKeyFor(p.jwt().secret().getBytes());
+    }
+
+    public String issueAccessToken(String subject) {
+        Date now = new Date();
+        Date expiry = new Date(now.getTime() + p.jwt().accessExpiry().toMillis());
+        return Jwts.builder()
+                .subject(subject) // username
+                .issuedAt(now) // 발행시간
+                .expiration(expiry) // 만료시간
+                .signWith(getSecretKey())
+                .compact();
+    }
 }
